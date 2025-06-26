6082a08a4498e814d3137416fa4242c0e307b780
Saverio976 <xavier.mitault@epitech.eu>
2025-06-26 22:51:42 +0100

    Fix

--- lua/telescope/_extensions/cross_live_grep/finder.lua
+++ lua/telescope/_extensions/cross_live_grep/finder.lua
@@ -21,39 +21,35 @@ _finders.cross_live_grep = function(opts)
   opts = opts or {}
   opts.pattern = opts.pattern or ''
 
-  local _cross_live_grep = function()
-    local results = {}
-    if opts.pattern ~= '' then
-      local results_tmp = _finders.scan.scan_dir(opts.path, {
-        hidden = true,
-        respect_gitignore = true,
-      })
+  local results = {}
+  if opts.pattern ~= '' then
+    local results_tmp = _finders.scan.scan_dir(opts.path, {
+      hidden = true,
+      respect_gitignore = true,
+    })
 
-      for _, cur_path in ipairs(results_tmp) do
-        local local_result = _finders.utils.grep_file(cur_path, opts.pattern)
-        for _, cur_match in ipairs(local_result) do
-          table.extend(results, cur_match)
-        end
+    for _, cur_path in ipairs(results_tmp) do
+      local local_result = _finders.utils.grep_file(cur_path, opts.pattern)
+      for _, cur_match in ipairs(local_result) do
+        table.extend(results, cur_match)
       end
     end
-
-    return _finders.finder.new_table({
-      results = results,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = function(tbl)
-            return _finders.to_relative(tbl.path, opts.cwd) .. ':' .. tbl.line_number .. ':' .. tbl.column_number
-          end,
-          path = entry[1],
-          line_number = entry[2],
-          column_number = entry[3],
-        }
-      end,
-    })
   end
 
-  return _cross_live_grep
+  return _finders.finder.new_table({
+    results = results,
+    entry_maker = function(entry)
+      return {
+        value = entry,
+        display = function(tbl)
+          return _finders.to_relative(tbl.path, opts.cwd) .. ':' .. tbl.line_number .. ':' .. tbl.column_number
+        end,
+        path = entry[1],
+        line_number = entry[2],
+        column_number = entry[3],
+      }
+    end,
+  })
 end
 
 return _finders.cross_live_grep
